55c659749eae0357ae3d13f3aacb97f16998098d
Polina <apolo3neorama>
2013-12-22 20:42:01 +0200

    TestSuite added

---
+++ src/Tests/TestSuite.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Polina on 12/22/13.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({QuickSortTest.class,BubbleSortTest.class,FactorisationTest.class})
+public class TestSuite {
+}
